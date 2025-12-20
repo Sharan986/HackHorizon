674ef3ba573cfc5356cd1ca0f4e282d02528d64a
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2025-12-20 08:41:56 +0530

    updated

--- src/components/ui/ClockElements/eventCardImages.tsx
+++ src/components/ui/ClockElements/eventCardImages.tsx
@@ -12,8 +12,20 @@ export function PlankImage() {
 
 export function EventCardShield() {
   return (
-    <div className="w-[91px] sm:w-[130px] md:w-[182px] lg:w-[260px]">
-        <Image src={Shield} alt="" className="w-full h-auto" draggable={false}/>
+    <div className="flex items-center gap-2 w-fit">
+      <div className="w-[91px] sm:w-[130px] md:w-[182px] lg:w-[260px]">
+        <Image src={Shield} alt="Shield" className="w-full h-auto" draggable={false}/>
+      </div>
+      <div className="w-[120px] sm:w-[160px] md:w-[200px] lg:w-[250px]">
+        <Image
+          src="/arka_jain_logo.png"
+          alt="ARKA JAIN University Logo"
+          width={250}
+          height={50}
+          className="w-full h-auto"
+          draggable={false}
+        />
+      </div>
     </div>
   );
 }
