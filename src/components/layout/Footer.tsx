483ed341d4ca2b0a99083fcda5028fd270083d26
Adeeb Razi <adeebrazi22@gmail.com>
2025-12-29 23:18:04 +0530

    Update footer contact information and links

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -41,11 +41,11 @@ export default function Footer() {
           </div>
           <div className="flex flex-col items-center">
             <p className="mb-2 md:mb-3 whitespace-pre-line text-center">
-              Dr.Souvik Singh Rathore
-              <br />Faculty Coordinator
+              MS Anjali Kumari
+              <br />Student Coordinator
             </p>
             <a
-              href={`tel:8809257089`}
+              href={`tel:6205120426`}
               className="relative flex justify-center items-center  gap-6 px-6 p-2 text-white  w-fit  shadow-[0_3px_0_#000] text-xl  md:text-2xl  font-medium border-t-4 border-b-4 border-b-[#EC8F01]  border-t-[#FFF] text-shadow  rounded-xl cursor-pointer transition-transform duration-200 hover:scale-[1.01] active:scale-95"
               style={{
                 background:
@@ -53,7 +53,7 @@ export default function Footer() {
               }}
 
             >
-              +91 88092 57089
+              +91 62051 20426
             </a>
           </div>
         </div>
@@ -63,7 +63,7 @@ export default function Footer() {
             <iframe
               title="Venue Location"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58842.60725308107!2d86.09784817698247!3d22.814949605614032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f5e5f1b496777f%3A0x1d506033b3ed835d!2sEmversity%20Campus%2C%20Jamshedpur!5e0!3m2!1sen!2sin!4v1766128633582!5m2!1sen!2sin"
-              width="200"
+              width="400"
               height="200"
               style={{ border: 0 }}
               allowFullScreen={true}
@@ -76,46 +76,26 @@ export default function Footer() {
       </div>
       <div className="w-full absolute bottom-0 px-4 sm:px-8 md:px-12 lg:px-20 py-3 md:py-4 border-t-2 border-[#FFFFFF1F] z-10 flex flex-col md:flex-row items-center justify-between gap-4 md:gap-2 bg-black/20 backdrop-blur-sm">
         <div className="text-white font-sans text-xs sm:text-sm md:text-base lg:text-xl text-center order-1 md:order-1">
-          Built in collaboration with GDGOC & CCS
+          Built in Collaboration with GDGoC AJU & CCS
         </div>
         <div className="text-white font-sans flex text-lg sm:text-xl md:text-2xl lg:text-xl gap-3 sm:gap-4 md:gap-5 order-2 md:order-2">
           <Link
-            href="mailto:innerve@aitpune.edu.in"
+            href="mailto:hackhorizon@arkajainuniversity.ac.in"
             target="_blank"
             rel="noopener noreferrer"
           >
             <MdOutlineMail className="hover:text-pink-500 transition-colors cursor-pointer" />
           </Link>
           <Link
-            href=""
+            href="https://www.instagram.com/hackhorizon_aju"
             target="_blank"
             rel="noopener noreferrer"
           >
             <FaInstagram className="hover:text-pink-500 transition-colors cursor-pointer" />
           </Link>
+          
           <Link
-            href="https://www.facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaMeta className="hover:text-blue-500 transition-colors cursor-pointer" />
-          </Link>
-          <Link
-            href="https://discord.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaDiscord className="hover:text-indigo-500 transition-colors cursor-pointer" />
-          </Link>
-          <Link
-            href="https://discord.gg"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaXTwitter className="hover:text-gray-400 transition-colors cursor-pointer" />
-          </Link>
-          <Link
-            href="https://github.com"
+            href="https://github.com/hackhorizon-aju"
             target="_blank"
             rel="noopener noreferrer"
           >
